4438d69e690e8a8ce991ee756b5ce0bf1c08f6e0
Nick <117512008+Niko-NN@users.noreply.github.com>
2023-04-11 13:15:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,9 +6,13 @@ from scipy.stats import norm
 chat_id = 5459656416 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    std_dev = (np.amax(x) - np.exp(1)) / (2 * norm.ppf((1 - p) / 2))
-    margin_of_error = std_dev / np.sqrt(n)
-    left_boundary = (np.mean(x) / 2) - margin_of_error
-    right_boundary = (np.mean(x) / 2) + margin_of_error
-    return (left_boundary, right_boundary)
+    t_i = 2  # время измерения
+    v = x / t_i  # вычисление выборочных значений скорости
+    n = len(v)
+    v_mean = np.mean(v)  # выборочное среднее скорости
+    s = np.sqrt(np.sum((v - v_mean) ** 2) / (n - 1))  # выборочное стандартное отклонение
+    norm_alpha_2 = norm.ppf(1 - p / 2, n - 1)  # квантиль распределения Стьюдента
+    a_hat = np.sum(v * t_i) / np.sum(t_i ** 2)  # оценка коэффициента ускорения
+    left_bound = a_hat - norm_alpha_2 * s / np.sqrt(np.sum(t_i ** 2))
+    right_bound = a_hat + norm_alpha_2 * s / np.sqrt(np.sum(t_i ** 2))
+    return (left_bound, right_bound)
